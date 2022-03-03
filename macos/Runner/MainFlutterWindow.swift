6d36029a5fad526562d62bb398f426b0bd3ef486
Marcin Slusarek <mrvrota@gmail.com>
2022-03-04 00:44:49 +0100

    Set window size using swift

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,7 +7,12 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    
+      self.styleMask = [.closable, .titled, .miniaturizable]
+      self.contentMaxSize = NSSize(width: 650, height: 650)
+      self.contentMinSize = NSSize(width: 650, height: 650)
+      self.setContentSize(NSSize(width: 650, height: 650))
+      
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
